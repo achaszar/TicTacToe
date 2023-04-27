d4a0c5e6a13c8737a499f92c5665f8c786eff8f9
Austin Chaszar <awchaszar@gmail.com>
2023-04-26 20:32:46 -0400

    Add project files.

---
+++ TicTacToe/Controllers/TicTacToeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class TicTacToeController : Controller
+    {
+        private TicTacToeViewModel _tttViewModel = new TicTacToeViewModel();
+        public IActionResult Index()
+        {
+            return View(_tttViewModel);
+        }
+
+        public IActionResult TicTacToeBoard(int wins, int loss, int tie)
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ TicTacToe/Models/TicTacToeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio.Models
+{
+    public class TicTacToeViewModel
+    {
+        public int Wins { get; set; }
+        public int Loss { get; set; }
+        public int Tie { get; set; }
+    }
+}
